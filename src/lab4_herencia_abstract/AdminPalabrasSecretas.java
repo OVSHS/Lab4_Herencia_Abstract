3f9cc33d1a5a2af45b6d9e2b69244abffc5fca46
Mario Enrique  Vasquez Adriano <marioenriquevasquez4@gmail.com>
2025-02-14 13:19:33 -0800

    Creando Clase AdminPalabrasSecretas

---
+++ src/lab4_herencia_abstract/AdminPalabrasSecretas.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_herencia_abstract;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Mario
+ */
+public class AdminPalabrasSecretas {
+     private String[] palabras;
+    private int count;
+    private Random random;
+    
+    public AdminPalabrasSecretas() {
+        palabras = new String[10];
+        count = 0;
+        random = new Random();
+    }
+    
+    public void agregarPalabra(String palabra) {
+        if (palabra != null && !palabra.isEmpty()) {
+            if (count >= palabras.length) {
+                String[] nuevo = new String[palabras.length * 2];
+                for (int i = 0; i < palabras.length; i++) {
+                    nuevo[i] = palabras[i];
+                }
+                palabras = nuevo;
+            }
+            palabras[count] = palabra;
+            count++;
+        }
+    }
+    
+    public String seleccionarPalabraAlAzar() {
+        if (count == 0) {
+            return "default";
+        }
+        int indice = random.nextInt(count);
+        return palabras[indice];
+    }
+}
